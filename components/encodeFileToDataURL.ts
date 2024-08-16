6821af36cb6f78b9e361b36c8f3b3bf81fb39559
Albert Brand <abrand@xebia.com>
2024-08-16 11:18:39 +0200

    Remove unused file

--- components/encodeFileToDataURL.ts
+++
@@ -1,7 +0,0 @@
-export const encodeFileToDataURL = (file: File) =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result as string);
-    reader.onerror = (error) => reject(error);
-  });

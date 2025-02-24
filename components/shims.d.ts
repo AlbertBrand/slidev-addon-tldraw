7ce5a7dcc60112f0bddb4708ddff3e672726c404
Albert Brand <abrand@xebia.com>
2025-02-24 14:29:11 +0100

    Add eslint and prettier, update ts config

--- components/shims.d.ts
+++ components/shims.d.ts
@@ -1,12 +1,4 @@
 declare global {
   const __DEV__: boolean;
-
-  interface ImportMeta {
-    hot: {
-      send: (event: string, data: any) => void;
-      on: (event: string, callback: (data: any) => void) => void;
-      off: (event: string, callback: (data: any) => void) => void;
-    };
-  }
 }
 export {};
